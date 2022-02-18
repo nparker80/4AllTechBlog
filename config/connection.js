3b7ee0eeb71cf6b6d8363ae44e731e01dbd3e430
Najla Parker <najla08@gmail.com>
2022-02-18 16:02:12 -0700

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,23 @@
+require('dotenv').config();
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
+let sequelize;
 
-const Sequelize = process.env.JAWSDB_URL
-? new Sequelize(process.env.JAWSDB_URL)
-: new equelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW), {
-  host: "localhost",
-  dialect: "mysql",
-    decimalNumbers: true,
-  },
-});
+if (process.env.JAWSDB_URL) {
+	sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+		{
+			host: 'localhost',
+			dialect: 'mysql',
+			port: 3306
+		}
+	);
+	};
+
+
 
 module.exports = sequelize;
